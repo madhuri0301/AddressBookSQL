e131eb003a2e50443757f1020ddbf021f86d9329
Madhuri <harshupatil0301@gmail>
2021-07-01 13:45:53 +0530

    [Madhuri]Add : Inserting Contacts To The Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,3 +13,9 @@ CREATE TABLE address_Book
 	phone VARCHAR(30)NOT NULL,
 	email VARCHAR(30)NOT NULL
 );
+
+INSERT INTO address_Book(first_name,last_name,address,city,state,zip,phone,email)
+VALUES ('Pranali','Patil','ShriKrishna Colony','Shahada','Maharashtra',435409,9879876800,'pranali01@gmail.com'),
+('Rupali','Patil','Tulsi Nagar','Shahada','Maharashtra',412309,9123654367,'rupaliPatil16@gmail.com'),
+('Riya','More','College Road','Nasik','Maharahtra',455110,8821056811,'riyamore@gmail.com'),
+('Priyanka','Sonar','Shvaji Nagar','Pune','Maharashtra',461124,7653809765,'priya03@gmail.com');
