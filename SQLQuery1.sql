c1c26a789e8831282accc4b3eb13d3bb74c92c57
Madhuri <harshupatil0301@gmail>
2021-07-01 13:31:02 +0530

    [Madhuri]Add : Creating Table And Adding Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE Address_Book_Service
 
 USE Address_Book_Service;
+
+CREATE TABLE address_Book
+(
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	address VARCHAR(30) NOT NULL,
+	city VARCHAR(30) NOT NULL,
+	state VARCHAR(30) NOT NULL,
+	zip VARCHAR(30) NOT NULL,
+	phone VARCHAR(30)NOT NULL,
+	email VARCHAR(30)NOT NULL
+);
